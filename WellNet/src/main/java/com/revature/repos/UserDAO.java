20f815bf9d986ffa6949a19c020def4325dc9b9d
Matthew Jordan <91514803+cal1bxr@users.noreply.github.com>
2021-11-19 15:35:56 -0500

    Delete WellNet directory

--- WellNet/src/main/java/com/revature/repos/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.models.User;
-
-public interface UserDAO extends JpaRepository<User, Integer>{
-	
-}
