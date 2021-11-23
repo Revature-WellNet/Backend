7c068dde1088e69a769d73da8b908c651ec315eb
Can Nguyen <vodanhchutieu@gmail.com>
2021-11-23 10:32:30 -0500

    done

--- src/main/java/com/revature/controllers/PatientController.java
+++ src/main/java/com/revature/controllers/PatientController.java
@@ -21,107 +21,10 @@ import com.revature.models.User;
 import com.revature.services.PatientService;
 
 
-<<<<<<< HEAD
-@CrossOrigin(origins="*", allowedHeaders="*") //these will most likely need to be specific for security to allow CORS.
-=======
 @CrossOrigin(origins = "http://localhost:4200")
->>>>>>> b7c5a77822ce21cbc7dbbe7ad2c43932f8d70110
 @RestController
 @RequestMapping(value = "/public/patient")
 public class PatientController {
-<<<<<<< HEAD
-	private PatientService patientService;
-	
-	@Autowired
-	public PatientController(PatientService patientService) {
-		this.patientService= patientService;
-	}
-	
-	
-	@GetMapping
-	public ResponseEntity<List<Patient>> findAllPatient(){
-		List<Patient> all = patientService.findAllPatients();
-		
-		if(all.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		}	
-		return ResponseEntity.ok(all);
-	}
-	
-	@GetMapping(value = "/id/{id}")
-	public ResponseEntity<Patient> getPatient(@PathVariable("id") int id) {
-		System.out.println("In get int id.");
-		Optional<Patient> patient = patientService.findPatientById(id);
-		if(patient.isPresent()) {
-			return ResponseEntity.ok(patient.get());
-		}
-		return ResponseEntity.noContent().build();
-	}
-	
-	@GetMapping(value = "/firstname/{firstName}")
-	public ResponseEntity<List<Patient>> getPatient(@PathVariable("firstname")String firstName){
-		System.out.println("In get string firstName.");
-		Optional<List<Patient>> list =  patientService.findPatientByName(firstName);
-		if(list.isPresent()) {
-			return ResponseEntity.ok(list.get());
-		}
-		return ResponseEntity.noContent().build();
-	}
-	
-	@GetMapping(value = "/fullname/{firstName}/{lastName}")
-	public ResponseEntity<List<Patient>> getPatient(@PathVariable("firstname")String firstName,
-			                 @PathVariable("lastname")String lastName) {
-		Optional<List<Patient>> list =  patientService.findPatientByName(firstName,lastName);
-		if(list.isPresent()) {
-			return ResponseEntity.ok(list.get());
-		}
-		return ResponseEntity.noContent().build();
-	}
-	
-	@GetMapping(value = "/fullnamedob/{firstName}/{lastName}/{dob}")
-	public ResponseEntity<Patient> getPatient(@PathVariable("firstname")String firstName,
-			                 @PathVariable("lastname")String lastName,
-			                 @PathVariable("dob")Date dob) {
-		Optional<Patient> patient =  patientService.findPatientByName(firstName,lastName,dob); 
-		if(patient.isPresent()) {
-			return ResponseEntity.ok(patient.get());
-		}
-		return ResponseEntity.status(204).build();
-	}
-	
-	@PostMapping
-	public  ResponseEntity<Patient> addPatient(@RequestBody Patient patient){
-		
-		if (patientService.addPatient(patient)) {
-			return ResponseEntity.status(201).build();
-		}else {
-			return ResponseEntity.status(400).build();
-		}
-		
-	}
-	
-	@PutMapping
-	public  ResponseEntity<Patient> updatePatient(@RequestBody Patient patient){
-		
-		if (patientService.updatePatient(patient)) {
-			return ResponseEntity.status(200).build();
-		}else {
-			return ResponseEntity.status(400).build(); 
-		}
-		
-	}
-	
-	@DeleteMapping(value = "/{id}")
-	public  ResponseEntity<Patient> deletePatient(@PathVariable("id")int id) {
-		
-		if (patientService.deletePatient(id)) {
-			return ResponseEntity.status(200).build();
-		}else {
-			return ResponseEntity.status(400).build(); 
-		}	
-	}
-	
-=======
 
     private PatientService patientService;
 
@@ -228,7 +131,6 @@ public class PatientController {
     }
 
 
->>>>>>> b7c5a77822ce21cbc7dbbe7ad2c43932f8d70110
 }
 	
 
