c78ff3ef4aef32c816b6cd34a0b658d551c286e9
Sam Zirbel <samuelzirbel@gmail.com>
2021-11-30 13:25:12 -0700

    Post Mapping Import

--- src/main/java/com/revature/controllers/userController.java
+++ src/main/java/com/revature/controllers/userController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
